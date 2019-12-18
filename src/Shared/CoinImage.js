724f19caa2c1166f46550118e1904341a01c2ace
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-12-18 21:40:16 +0100

    - add s to http

--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -4,7 +4,7 @@ export default function ({coin, style}) {
     return <img
         alt={coin.CoinSymbol}
         style={style || {height: '50px'}}
-        src={`http://cryptocompare.com${
+        src={`https://cryptocompare.com${
             coin.ImageUrl
         }`}
     />;
