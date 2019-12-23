05e7a08ed29b7033fca8dde81f9c601109adc9be
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-12-23 11:43:53 +0100

    - finish dashboard grid

--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -1,8 +1,9 @@
 import React from "react";
-import styled, {css} from 'styled-components';
+import styled, {css} from "styled-components";
 import {SelectableTile} from "../Shared/Tile";
-import {fontSize3, fontSizeBig} from "../Shared/Styles";
+import {fontSize3, fontSizeBig, greenBoxShadow} from "../Shared/Styles";
 import {CoinHeaderGridStyled} from "../Settings/CoinHeaderGrid";
+import {AppContext} from "../App/AppProvider";
 
 const JustifyRight = styled.div`
   justify-self: right;
@@ -12,82 +13,103 @@ const JustifyLeft = styled.div`
   justify-self: left;
 `;
 
-const TicketPrice = styled.div`
-  ${fontSizeBig};
-`;
-
-const ChangeSym = styled.div`
-    color: blue;
+const TickerPrice = styled.div`
+  ${fontSizeBig}
 `;
 
 const ChangePct = styled.div`
-    color: green;
-    ${props => props.red && css`
-        color: red;
+  color: green;
+  ${props =>
+    props.red &&
+    css`
+      color: red;
     `}
 `;
 
-const numberFormat = number => {
-    return +(number + '').slice(0, 7)
-};
+const ChangeSymbol = styled.div`
+  color: blue;
+`;
+
+const numberFormat = number => +(number + "").slice(0, 7);
 
 const PriceTileStyled = styled(SelectableTile)`
-   ${props => props.compact && css`
-        display: grid;
-        ${fontSize3}
-        grid-gap: 5px;
-        grid-template-columns: repeat(3, 1fr);
-   `}
+  ${props =>
+    props.compact &&
+    css`
+      display: grid;
+      ${fontSize3}
+      grid-gap: 5px;
+      grid-template-columns: repeat(3, 1fr);
+      justify-items: right;
+    `}
+
+  ${props =>
+    props.currentFavorite &&
+    css`
+      ${greenBoxShadow}
+      pointer-events: none;
+    `}
 `;
 
 function ChangePercent({data}) {
-    return (<JustifyRight>
+    return (
+        <JustifyRight>
             <ChangePct red={data.CHANGEPCT24HOUR < 0}>
-                {numberFormat(data.CHANGEPCT24HOUR)}
+                {numberFormat(data.CHANGEPCT24HOUR)}%
             </ChangePct>
         </JustifyRight>
-    )
+    );
 }
 
-function PriceTile({sym, data}) {
+function PriceTileTag({sym, data, currentFavorite, setCurrentFavorite}) {
     return (
-        <PriceTileStyled>
+        <PriceTileStyled
+            onClick={setCurrentFavorite}
+            currentFavorite={currentFavorite}
+        >
             <CoinHeaderGridStyled>
-                <div>
-                    <ChangeSym>
-                        {sym}
-                    </ChangeSym>
-                </div>
+                <ChangeSymbol>{sym}</ChangeSymbol>
                 <ChangePercent data={data}/>
             </CoinHeaderGridStyled>
-            <TicketPrice>
-                ${numberFormat(data.PRICE)}
-            </TicketPrice>
+            <TickerPrice>${numberFormat(data.PRICE)}</TickerPrice>
         </PriceTileStyled>
-    )
+    );
 }
 
-function PriceTileCompact({sym, data}) {
+function PriceTileCompactTag({
+                                 sym,
+                                 data,
+                                 currentFavorite,
+                                 setCurrentFavorite
+                             }) {
     return (
-        <PriceTileStyled compact>
-                <JustifyLeft>
-                    <ChangeSym>
-                        {sym}
-                    </ChangeSym>
-                </JustifyLeft>
-                <ChangePercent data={data}/>
-            <div>
-                ${numberFormat(data.PRICE)}
-            </div>
+        <PriceTileStyled
+            onClick={setCurrentFavorite}
+            compact
+            currentFavorite={currentFavorite}
+        >
+            <JustifyLeft>{sym}</JustifyLeft>
+            <ChangePercent data={data}/>
+            <div>${numberFormat(data.PRICE)}</div>
         </PriceTileStyled>
-    )
+    );
 }
 
 export default function ({price, index}) {
     let sym = Object.keys(price)[0];
-    let data = price[sym]['USD'];
-    let TileClass = index < 2 ? PriceTile : PriceTileCompact;
-    return <PriceTile
-        sym={sym} data={data}>
-    </PriceTile>
+    let data = price[sym]["USD"];
+    let TileClass = index < 5 ? PriceTileTag : PriceTileCompactTag;
+
+    return (
+        <AppContext.Consumer>
+            {({currentFavorite, setCurrentFavorite}) => (
+                <TileClass
+                    sym={sym}
+                    data={data}
+                    currentFavorite={currentFavorite === sym}
+                    setCurrentFavorite={() => setCurrentFavorite(sym)}
+                />
+            )}
+        </AppContext.Consumer>
+    );
 }
